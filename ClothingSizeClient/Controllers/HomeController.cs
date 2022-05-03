f44302dc391f84e84bf1462025c6bd659ca0e293
Amy Young <youngamy1223@gmail.com>
2022-05-02 20:09:54 -0600

    changed some styling and layout
    Co-authored-by: Zoe Weinstein <zweinstein@gmail.com>
    Co-authored-by: Jonathan Delcid <delcidj22@users.noreply.github.com>
    Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>
    Co-authored-by: ChrisNakayama <ChrisNakayama@users.noreply.github.com>

--- ClothingSizeClient/Controllers/HomeController.cs
+++ ClothingSizeClient/Controllers/HomeController.cs
@@ -53,16 +53,16 @@ namespace ClothingSizeClient.Controllers
 
 
 
-       // public IActionResult Privacy()
-     //   {
-      //      return View();
-      //  }
+       public IActionResult Privacy()
+       {
+           return View();
+       }
 
-       // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-         //   return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
+    //    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //     public IActionResult Error()
+    //     {
+    //        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    //     }
     }
 }
 
