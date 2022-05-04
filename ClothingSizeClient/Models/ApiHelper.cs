14705423298a406ff9078a90bfe5ad66622de8bb
Amy Young <youngamy1223@gmail.com>
2022-05-03 20:24:42 -0600

    clean up endpoints list and formatting
    Co-authored-by: Zoe Weinstein <zweinstein@gmail.com>
    Co-authored-by: Chris Nakayama <cnakayam@gmail.com>
    Co-authored-by:  Wajma Niazi  <w1niazi@hotmail.com>
    Co-authored-by: Jonathan Delcid <jdelcid23@gmail.com>

--- ClothingSizeClient/Models/ApiHelper.cs
+++ ClothingSizeClient/Models/ApiHelper.cs
@@ -5,13 +5,6 @@ namespace ClothingSizeClient.Models
 {
   class ApiHelper
   {
-    // public static async Task<string> GetAll()
-    // {
-    //   RestClient client = new RestClient("http://localhost:5000/api");
-    //   RestRequest request = new RestRequest($"Brands", Method.GET);
-    //   var response = await client.ExecuteTaskAsync(request);
-    //   return response.Content;
-    // }
 
     public static async Task<string> GetUserInput(string userInput)
     {
